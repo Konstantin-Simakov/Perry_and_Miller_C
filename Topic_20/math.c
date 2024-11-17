6138e4b77be8ebe063d8f1dab6d8da27f8e4e3ea
Konstantin_Simakov <kvs.2000@mail.ru>
2024-11-17 20:20:37 +0300

    Add files via upload

--- Topic_20/math.c
+++ Topic_20/math.c
@@ -4,11 +4,11 @@
 #include <string.h>
 #include <math.h>
 
-#define PI 3.14159
+// #define PI 3.14159
 
 int main(void)
 {
-	// const double PI = atan(1) * 4;
+	const double PI = atan(1) * 4;
 
 	printf("Let\'s start homework!\n");
 	
@@ -20,7 +20,7 @@ int main(void)
 	printf("\nTopic 2: Powers\n");
 	printf("4^3 = %.1f\n", pow(4.0, 3.0));
 	printf("7^5 = %.1f\n", pow(7.0, 5.0));
-	printf("34^(1/2) = %.1f\n", pow(34.0, 0.5));
+	printf("36^(1/2) = %.1f\n", pow(36.0, 0.5));
 
 	printf("\nTopic 3: Trigonometry\n");
 	printf("Cos of 60 degrees = %.3f\n", 
@@ -29,18 +29,16 @@ int main(void)
 		sin(90 * (PI / 180)));
 	printf("Tg of 75 degrees = %.3f\n", 
 		tan(75 * (PI / 180)));
-	printf("Arccos of 45 degrees = %.3f\n", 
-		acos(45 * (PI / 180)));
-	printf("Arcsin of 45 degrees = %.3f\n", 
-		asin(30 * (PI / 180)));
-	printf("Arctg of 15 degrees = %.3f\n", 
-		atan(15 * (PI / 180)));
+	printf("Arccos of 0.5 = %.3f\n", acos(0.5));
+	printf("Arcsin of 0.5 = %.3f\n", asin(0.5));
+	printf("Arctg of 1 = %.3f\n", atan(1));
 
-
-	printf("\nTopic 4: Logarithmic funcitons\n");
+	printf("\nTopic 4: Logarithmic functions\n");
 	printf("e^2 = %.3f\n", exp(2));
 	printf("ln5 = %.3f\n", log(5));
 	printf("lg5 = %.3f\n", log10(5));
+
+	// Add ceil(), floor(), fabs() examples.
 
 	return 0;
 }
